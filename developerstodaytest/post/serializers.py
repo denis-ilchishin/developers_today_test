d8fc93668cca6f8595df2685edaf51221434c23c
Denis Ilchishin <ilchishin.denis@gmail.com>
2021-01-18 05:20:08 +0000

    feat: add post comment serializers

--- developerstodaytest/post/serializers.py
+++ developerstodaytest/post/serializers.py
@@ -1,7 +1,7 @@
 from django.utils.timezone import now
 from rest_framework import serializers
 
-from .models import Post
+from .models import Post, PostComment
 
 
 class PostSerializerMeta:
@@ -31,8 +31,61 @@ class UpdatePostSerializer(serializers.ModelSerializer):
 
 
 class ViewPostSerializer(serializers.ModelSerializer):
-
     author = serializers.CharField(source="author.username")
 
     class Meta(PostSerializerMeta):
         fields = ("title", "author", "date_created", "date_modified", "url")
+
+
+class PostCommentSerializerMeta:
+    model = PostComment
+
+
+class PostCommentPostDefault:
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        return serializer_field.context["post"]
+
+    def __repr__(self):
+        return "%s()" % self.__class__.__name__
+
+
+class CreatePostCommentSerializer(serializers.ModelSerializer):
+    content = serializers.CharField(
+        max_length=PostComment._meta.get_field("content").max_length
+    )
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    post = serializers.HiddenField(default=PostCommentPostDefault())
+
+    class Meta(PostCommentSerializerMeta):
+        fields = (
+            "post",
+            "content",
+            "author",
+        )
+
+
+class UpdatePostCommentSerializer(serializers.ModelSerializer):
+    content = serializers.CharField(
+        max_length=PostComment._meta.get_field("content").max_length
+    )
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    post = serializers.HiddenField(default=PostCommentPostDefault())
+    date_modified = serializers.HiddenField(default=now)
+
+    class Meta(PostCommentSerializerMeta):
+        fields = ("content", "author", "post", "date_modified")
+
+
+class ViewPostCommentSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source="author.username")
+
+    class Meta(PostCommentSerializerMeta):
+        fields = (
+            "content",
+            "author",
+            "post",
+            "date_created",
+            "date_modified",
+        )
