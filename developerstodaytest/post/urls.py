47788ab52a07a17b004ae647cf09d80a55591350
Denis Ilchishin <ilchishin.denis@gmail.com>
2021-01-18 05:21:19 +0000

    feat: add post comment nested router and its routes

--- developerstodaytest/post/urls.py
+++ developerstodaytest/post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls.conf import include
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
 from . import views
 
@@ -9,4 +10,11 @@ app_name = "posts"
 router = SimpleRouter()
 router.register("post", views.PostsViewset, "post")
 
-urlpatterns = [path("", include(router.urls))]
+
+posts_router = routers.NestedSimpleRouter(router, "post", lookup="post")
+posts_router.register("comment", views.PostCommentsViewset, basename="comment")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(posts_router.urls)),
+]
